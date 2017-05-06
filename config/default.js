792ffe55be37a76117e95204e551a94756802869
Zhmoll <zhmoll@zhmoll.com>
2017-05-06 19:59:17 +0800

    修复aeskey

--- config/default.js
+++ config/default.js
@@ -6,8 +6,8 @@ module.exports = {
     appid: 'wxb54a4c0517a8584e',
     appsecret: '40c9a95eac50c41f4df617563cbb08ea',
     token: 'zhmollcourseclouds',
-    encodingAESKey: 'encodinAESKey',
-    checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+    // encodingAESKey: 'encodinAESKey',
+    checkSignature: false // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
   },
   session: {
     secret: 'ZhmollCourseClouds',
