7a166a8b774940aaf00a1377f5582555318e8bd3
Ali <1102914569@qq.com>
2017-05-07 20:32:28 +0800

    更新了移动端文件

--- public/user-center/assets/js/register.js
+++ public/user-center/assets/js/register.js
@@ -6,32 +6,31 @@ function getQueryString(name) {
 }
 
 document.ready = function click_button() {
-        $("button").click(function cc() {
-            //alert(getQueryString('code'));
-            if(document.getElementById("id").value==''||$("#password").val()=='')
-            {
-                alert("用户名和密码不能为空");
-                return;
-            }
+    $("button").click(function cc() {
+        //alert(getQueryString('code'));
+        if (document.getElementById("id").value == '' || $("#password").val() == '') {
+            alert("用户名和密码不能为空");
+            return;
+        }
 
-            var data = {
-                "uid": document.getElementById("id").value,
-                "university": $('#selected option:selected').val(),
-                "password": $("#password").val(),
-                "code": getQueryString("code"),
-                "state": getQueryString("state")
-            };
-            $.post("http://112.74.13.107/api/profile/wechat/bind", data, function (data, state) {
-                if (data.code == 2003) {
-                    alert("绑定微信成功");
-                    localStorage.id=data.body.id;
-                    localStorage.name=data.body.name;
-                    localStorage.nickname=data.body.nickname;
-                    localStorage.signin=true;
-                    window.location.href = "index.html";
-                }
-                else alert(data.message);
-            })
+        var data = {
+            "uid": document.getElementById("id").value,
+            "university": $('#selected option:selected').val(),
+            "password": $("#password").val(),
+            "code": getQueryString("code"),
+            "state": getQueryString("state")
+        };
+        $.post("http://courseclouds.zhmoll.com/api/profile/wechat/bind", data, function (data, state) {
+            if (data.code == 2003) {
+                alert("绑定微信成功");
+                localStorage.id = data.body.id;
+                localStorage.name = data.body.name;
+                localStorage.nickname = data.body.nickname;
+                localStorage.signin = true;
+                window.location.href = "index.html";
+            }
+            else alert(data.message);
         })
-    }
-    ;
+    })
+}
+;
