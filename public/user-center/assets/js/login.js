a1b65d95fbf48d385cc3bf7faecec964b5001a71
Zhmoll <zhmoll@zhmoll.com>
2017-05-08 19:45:46 +0800

    试试看

--- public/user-center/assets/js/login.js
+++ public/user-center/assets/js/login.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
   $('button').click(function () {
+    var university = $('#university').text();
+    var uid = $('#uid').text();
+    var password = $('#password').text()
     $.post('../api/users/login', {
-      university: $('#university').text(),
-      uid: $('#uid').text(),
-      password: $('#password').text()
+      university: university,
+      uid: uid,
+      password: password
     }, function (data, status) {
       console.log(data);
       alert(data.message);
