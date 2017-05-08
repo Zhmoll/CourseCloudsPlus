ecf12440e96c3bb242731a0bc0eb466d0d0d6d32
Zhmoll <zhmoll@zhmoll.com>
2017-05-08 19:43:34 +0800

    登录接口

--- public/user-center/assets/js/login.js
+++ public/user-center/assets/js/login.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   $('button').click(function () {
     $.post('../api/users/login', {
-      university: $('#user-university').text(),
-      uid: $('#user-name').text(),
-      password: $('#user-password').text()
+      university: $('#university').text(),
+      uid: $('#uid').text(),
+      password: $('#password').text()
     }, function (data, status) {
       console.log(data);
       alert(data.message);
