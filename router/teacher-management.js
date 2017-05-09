164b78d8ad8b3cf980af5c7210c22e320275455e
张效伟 <zhmoll@zhmoll.com>
2017-05-10 00:11:13 +0800

    教师登录

--- router/teacher-management.js
+++ router/teacher-management.js
@@ -3,6 +3,26 @@ const router = express.Router();
 const OAuthApi = require('../wechat/OAuth');
 const path = require('path');
 
-router.use(express.static(path.join(__dirname,'../public/teacher-management')));
+function checkLogin(req, res, next) {
+  if (!req.session.userid) {
+    req.session.url = path.join('http://courseclouds.zhmoll.com/', req.originalUrl);
+    const url = OAuthApi.getAuthorizeURL('http://courseclouds.zhmoll.com/user-center/wechat_login', 'wechat_login', 'snsapi_base');
+    res.redirect(url);
+    return;
+  }
+  next();
+}
 
+router.get('/addcourse.html', checkLogin);
+router.get('/addcoursedetail.html', checkLogin);
+router.get('/calendar.html', checkLogin);
+router.get('/chart.html', checkLogin);
+router.get('/form.html', checkLogin);
+router.get('/index.html', checkLogin);
+router.get('/leave.html', checkLogin);
+router.get('/sign-up.html', checkLogin);
+router.get('/table-list.html', checkLogin);
+router.get('/tables.html', checkLogin);
+
+router.use(express.static(path.join(__dirname, '../public/teacher-management')));
 module.exports = router;
