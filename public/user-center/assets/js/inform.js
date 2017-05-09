7207f8e4c786013d1953b249cbeea6baf62d0398
Ali <1102914569@qq.com>
2017-05-09 19:57:42 +0800

    no message

--- public/user-center/assets/js/inform.js
+++ public/user-center/assets/js/inform.js
@@ -38,10 +38,10 @@ window.onload = function () {
         var coursetimes = data.body; // 是数组
         for (i = 0; i < coursetimes.length; i++) {
             if (coursetimes[i].id == coursetimeid) {
-                $("#coursetime").append('<span>第' + coursetimes[i].weekday + '周 第' + coursetimes[i].rows.join(",") + '节 ' + course.teachers[i].name + '</span> ');
+                $("#coursetime").append('<span class="data">第' + coursetimes[i].weekday + '周 第' + coursetimes[i].rows.join(",") + '节 ' + course.teachers[i].name + '</span> ');
             }
             else {
-                $("#coursetime").append('<span style="color: red">第' + coursetimes[i].week + '周 周' + coursetimes[i].weekday == 0 ? 7 : coursetimes[i].weekday + '第' + coursetimes[i].rows.join(',') + '节</span> ');
+                $("#coursetime").append('<span class="data" style="color: red">第' + coursetimes[i].week + '周 周' + coursetimes[i].weekday == 0 ? 7 : coursetimes[i].weekday + '第' + coursetimes[i].rows.join(',') + '节</span> ');
             }
         }
     });
