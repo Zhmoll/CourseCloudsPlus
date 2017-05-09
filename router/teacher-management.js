39e7c8df10f7ecf9e3f5e4a1fb9d4e333c2137ce
Ali <1102914569@qq.com>
2017-05-09 09:52:03 +0800

    暴露教师端

--- router/teacher-management.js
+++ router/teacher-management.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const OAuthApi = require('../wechat/OAuth');
+const path = require('path');
 
-router.use(express.static('../public'));
+router.use(express.static(path.join(__dirname,'../public/teacher-management')));
 
 module.exports = router;
