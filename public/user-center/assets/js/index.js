aee7d83f749ff34b3c39bddb429cb3d10fdca79e
Ali <1102914569@qq.com>
2017-05-09 17:51:40 +0800

    no message

--- public/user-center/assets/js/index.js
+++ public/user-center/assets/js/index.js
@@ -1,4 +1,7 @@
 window.onload = function () {
     $("#name").text(localStorage.name);
     $(".title_imagine").css = {"background-image": "url(" + localStorage.profile + ")"};
+    $("#course").click(function () {
+        window.location.href='course.html'
+    })
 };
