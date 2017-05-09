8b529c9603873f8625742715225428d392183a21
Ali <1102914569@qq.com>
2017-05-09 22:19:58 +0800

    no message

--- public/user-center/assets/js/inform.js
+++ public/user-center/assets/js/inform.js
@@ -61,7 +61,6 @@ window.onload = function () {
                     weekday = '周六';
                     break;
             }
-            var element = '<span class="data">第' + coursetimes[i].week + '周 ' + weekday + ' 第' + coursetimes[i].rows.join(',') + '节</span> ';
             if (coursetimes[i]._id == coursetimeid) {
                 $("#coursetime").append('<p><span  style="color: red;">第' + coursetimes[i].week + '周 ' + weekday + ' 第' + coursetimes[i].rows.join(',') + '节</span> </p>');
             }
@@ -93,9 +92,7 @@ window.onload = function () {
             var date=data.body[i].createdAt;
             var title = data.body[i].title;
             var teacher = data.body[i].from.name;
-
             $.get("../api/notices/inbox/" + noticeid, function (data1) {
-                console.log(data1.body);
                 var content = data1.body.notice.content;
                 $("#coursemessage").append('<li class="ui-border-t"><p><span>标题：</span><span class="date">' + title + '</span></p> <p><span>发送教师：</span><span class="date">' + teacher + '</span></p> <p><span>发送时间：</span><span class="date">' + date + '</span></p> <p><span>内容：</span><span class="date">' + content + '</span></p> </li>')
             })
@@ -108,5 +105,50 @@ window.onload = function () {
             console.log(data);
             return;
         }
+        for (i = 0; i < data.body.length; i++) {
+            console.log(data.body[i]);
+            // var leave_id = data.body[i]._id;
+            // var date=data.body[i].createdAt;
+            // var title = data.body[i].title;
+            // var teacher = data.body[i].from.name;
+            //
+            //     var content = data1.body.notice.content;
+            //     var leave = data.body; // 是数组
+            //     for (i = 0; i < coursetimes.length; i++) {
+            //         var weekday;
+            //         switch (coursetimes[i].weekday) {
+            //             case 0:
+            //                 weekday = '周日';
+            //                 break;
+            //             case 1:
+            //                 weekday = '周一';
+            //                 break;
+            //             case 2:
+            //                 weekday = '周二';
+            //                 break;
+            //             case 3:
+            //                 weekday = '周三';
+            //                 break;
+            //             case 4:
+            //                 weekday = '周四';
+            //                 break;
+            //             case 5:
+            //                 weekday = '周五';
+            //                 break;
+            //             case 6:
+            //                 weekday = '周六';
+            //                 break;
+            //         }
+            //         if (coursetimes[i]._id == coursetimeid) {
+            //             $("#coursetime").append('<p><span  style="color: red;">第' + coursetimes[i].week + '周 ' + weekday + ' 第' + coursetimes[i].rows.join(',') + '节</span> </p>');
+            //         }
+            //         else {
+            //             $("#coursetime").append('<p><span >第' + coursetimes[i].week + '周 ' + weekday + ' 第' + coursetimes[i].rows.join(',') + '节</span> </p>');
+            //         }
+            //     }
+            //     $("#myleave").append('<li class="ui-border-t"><p><span>课程号：</span><span class="date">' + title + '</span></p> <p><span>课程名：</span><span class="date">' + teacher + '</span></p> <p><span>上课时间：</span><span class="date">' + date + '</span></p> <p><span>原因：</span><span class="date">' + content + '</span></p><p><span style="color: red">已允许</span></p></li>')
+
+        }
+
     });
 };
