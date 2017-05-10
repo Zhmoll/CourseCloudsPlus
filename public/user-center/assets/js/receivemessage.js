4b7f58ed080d0fe2ba627bc933c73b23146048e8
Zhmoll <zhmoll@zhmoll.com>
2017-05-10 12:11:36 +0800

    收件箱全部消息

--- public/user-center/assets/js/receivemessage.js
+++ public/user-center/assets/js/receivemessage.js
@@ -52,14 +52,42 @@ window.onload = function () {
     }
     else {
         // 显示收件箱
-        $.get("../api/notices/inbox", function (data1) {
+        $.get("../api/notices/inbox", function (data) {
             if (data1.code != 2305) {
                 $("#div1").append("<ul class=" + "ul1" + " id='ul2'" + "> <li class=" + "ui-border-t " + "id=" + "li1" + "> <p><span>" + "</span><span class=" + "date>" + "</span></p><h4>" + "无消息" + "</h4></li></ul>");
                 $(".ul1").addClass("ui-list ui-list-pure ui-border-tb");
                 alert(data1.message);
                 return;
             }
+            var notices = data.body;
+            var element = '<ul class="ul1" id="ul2">';
 
+            notices.forEach(function (notice) {
+                var item = '<li class="ui-border-t">';
+                var title = notice.title;
+                var time = moment(notice.createdAt).format('YY年MM月DD日 HH:mm:ss');
+                item += '<span>标题：' + title + '</span><br />';
+                item += '<span>时间：' + time + '</span><br />';
+                var course = notice.course;
+                var from_name = notice.from.nickname;
+                var from_id = notice.from.id;
+                var from_avatar = notice.from.avatar;
+                if (course) {
+                    // 如果信件是由教师群发的
+                    for (var i = 0; i < course.teachers.length; i++) {
+                        if (course.teachers[i].id == from_id) from_name = course.teachers[i].name;
+                    }
+                    item += '<span>课程：<a href="http://courseclouds.zhmoll.com/user-center/inform.html?courseid='
+                        + course._id + '">' + course.cid + ' - ' + course.name + '</a></span><br />';
+                }
+                item += '<span>来自：<a href="http://courseclouds.zhmoll.com/user-center/profile.html?userid='
+                    + from_id + '">' + from_name + '</a></span><br />';
+                item += '</li>';
+                element += item;
+            });
+            element += '</ul>';
+            $("#div1").append(element);
+            $(".ul1").addClass("ui-list ui-list-pure ui-border-tb");
         });
     }
     $(".ul1").css({
