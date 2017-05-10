4d711a8039a0842b1d9250e7b9f32a5ba8c3691b
Ali <1102914569@qq.com>
2017-05-10 08:47:44 +0800

    用户个人信息页面测试

--- public/user-center/assets/js/profile.js
+++ public/user-center/assets/js/profile.js
@@ -1,4 +1,14 @@
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return decodeURI(r[2]);
+    return null;
+}
 window.onload = function () {
     $("#name").text(localStorage.name);
     $("#title_imagine").attr("src",localStorage.profile);
+    var userid = getQueryString('userid');
+    $.get("..//api/users/"+userid,function (data) {
+        console.log(data.body);
+    })
 };
