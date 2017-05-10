0ff1a67a12a94b09c6d96d443df357eb1be2d511
Zhmoll <zhmoll@zhmoll.com>
2017-05-10 09:53:50 +0800

    修复一个错误

--- public/user-center/assets/js/receivemessage.js
+++ public/user-center/assets/js/receivemessage.js
@@ -44,9 +44,9 @@ window.onload = function () {
                     + data.body.notice.course.id + '">' + data.body.notice.course.cid + ' - '
                     + data.body.notice.course.name + '</a></span></li>';
             }
-            var from_name = data.body.from.nickname;
-            var from_id = data.body.from.id;
-            var from_avatar = data.body.from.avatar;
+            var from_name = data.body.notice.from.nickname;
+            var from_id = data.body.notice.from.id;
+            var from_avatar = data.body.notice.from.avatar;
 
             element += '<li class="ui-border-t"><span>来自：<a href="http://courseclouds.zhmoll.com/user-center/profile.html?userid=?"'
                 + from_id + '">' + from_name + '</a></span></li>';
